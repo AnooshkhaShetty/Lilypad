019e2067f9ee265d302b32ea6a19d998babec69b
beam7 <52757011+beam7@users.noreply.github.com>
2023-01-28 17:36:15 -0800

    Add files via upload
    merging ashish and my code

--- src/App.js
+++ src/App.js
@@ -7,8 +7,30 @@ import About from './pages/about';
 import logo from './logo.svg';
 // import lilypadlogo from './lilypadlogo.png';
 import './App.css';
+import {db} from './config.js';
+import { doc, setDoc, getDoc } from "firebase/firestore"; 
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins"></link>
+
+const addSomething = async () => {
+  // Add a new document in collection "cities"
+  await setDoc(doc(db, "sellers", "Ashish"), {
+    name: "Ashish",
+    state: "CA",
+    country: "USA",
+    selling: {"radashishes": 12}
+  });
+}
+const getSomething = async () => {
+  const docRef = doc(db, "sellers", "Ashish");
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    alert(docSnap.data().selling.radashishes)
+  } else {
+    alert("no doc :(")
+  }
+}
 
 function App() {
   return(
